effcca2f39411c8c738a3668fda41c70b02fe5fc
shaunlsy <shaunlowshuennyuh@hotmail.com>
2019-12-11 11:11:17 +0000

    Boris Bike Challenge 10

---
+++ lib/dockingstation.rb
@@ -0,0 +1,18 @@
+require 'bike'
+
+class DockingStation
+  # def initialize
+  #   # @bike = 1
+  # end 
+  
+  def release_bike
+    Bike.new 
+  end 
+end 
+
+# docking_station = DockingStation.new 
+# puts "docking station"
+# p docking_station
+
+# puts "release bike"
+# p docking_station.release_bike
